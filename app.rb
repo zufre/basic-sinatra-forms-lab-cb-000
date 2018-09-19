0edd94fabe01b743335cbfc5b2f4fda69bf6f346
Cedric David <cedric.david@btopenworld.com>
2018-09-19 06:39:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,4 +7,17 @@ get '/newteam' do
   erb :newteam
 end
 
+post '/newteam' do
+  
+  @name = params[:name]
+  @coach = params[:coach]
+  @pointGuard = params[:pointGuard]
+  @shootingGuard = params[:shootingGuard]
+  @smallForward = params[:smallForward]
+  @powerForward = params[:powerForward]
+  @center = params[:center]
+  
+  erb :team
+end
+
 end
